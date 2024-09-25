488c0828ff347a4379de0ea9b24746d2628ebfb0
Anukamal01 <134486507+Anukamal01@users.noreply.github.com>
2024-09-25 05:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,5 @@ def predict():
     except Exception as e:
         return render_template('index.html', error=str(e))
 
-
-
-app.run(port=5001)
+if __name__ == '__main__':
+    app.run(debug=True)
